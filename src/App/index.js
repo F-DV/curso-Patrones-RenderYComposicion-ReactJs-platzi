7b905d1f87d0036e5bb5f61a1589a2e98f61ed15
FelipeQG <fquiceno1@misena.edu.co>
2022-04-27 23:09:34 -0500

    Modificacion ara no dejar buscar mientras cargan los todos

--- src/App/index.js
+++ src/App/index.js
@@ -34,12 +34,14 @@ function App() {
       <TodoHeader>
         <TodoCounter
           totalTodos={totalTodos}
-          completedTodos={completedTodos}    
+          completedTodos={completedTodos}
+          loading={loading}    
         />
         
         <TodoSearch
         searchValue={searchValue}
         setSearchValue={setSearchValue}
+        loading={loading}
         />
       </TodoHeader>
 
